c2f41178471023ed1729bd0820f4aeab91406577
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-05-13 01:14:45 +0330

    initial helpers

--- index.php
+++ index.php
@@ -8,5 +8,33 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'project');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-
+// helpers
+function protocol()
+{
+    return stripos(
+        $_SERVER['SERVER_PROTOCOL'],
+        'https'
+    ) === true ? 'https://' : 'http://';
+}
+exit;
+function currentDomain()
+{
+    return protocol() . $_SERVER['HTTP_HOST'];
+}
+function asset($src)
+{
+    $domain = trim(CURRENT_DOMAIN, '/');
+    $src = $domain . '/' . trim($src . '/');
+    return $src;
+}
+function url($url)
+{
+    $domain = trim(CURRENT_DOMAIN, '/');
+    $url = $domain . '/' . trim($url . '/');
+    return $url;
+}
+function currentUrl()
+{
+    return currentDomain() . $_SERVER['REQUEST_URI'];
+}
 ?>
