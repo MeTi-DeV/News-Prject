0cc098619e81cc4eb10ca182fa018b23a0706981
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-05-18 23:33:09 +0330

    add Routing Project

--- index.php
+++ index.php
@@ -9,6 +9,38 @@ define('DB_NAME', 'project');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 // helpers
+// Routing Function
+function uri($reservedUrl, $class, $method, $requestMethod = 'GET')
+{
+    $parameters = [];
+    $currentUrl = explode('?', currentUrl())[0];
+    $currentUrl = str_replace(CURRENT_DOMAIN, '', $currentUrl);
+    $currentUrl = trim($currentUrl, '/');
+    $currentUrlArray = explode('/', $currentUrl);
+    $currentUrlArray = array_filter($currentUrlArray);
+    //reserve URL
+
+    $reservedUrl = trim($reservedUrl, '/');
+    $reservedUrlArray = explode('/', $reservedUrl);
+    $reservedUrlArray = array_filter($reservedUrlArray);
+    if (sizeof($currentUrlArray) != sizeof($reservedUrlArray) || methodField() != $requestMethod) {
+        return false;
+    }
+    for ($key = 0; $key < sizeof($currentUrlArray); $key++) {
+        if ($reservedUrlArray[$key][0] == "{" && $reservedUrlArray[$key][strlen($reservedUrlArray[$key]) - 1] == "}") {
+            array_push($parameters, $currentUrlArray[$key]);
+        } elseif ($currentUrlArray[$key] !== $reservedUrlArray[$key]) {
+            return false;
+        }
+    }
+    if (methodField() == 'POST') {
+        $request = isset($_FILES) ? array_merge($_POST, $_FILES) : $_POST;
+        $parameters = array_merge([$request], $parameters);
+    }
+    $object = new $class;
+    call_user_func_array(array($object, $method), $parameters);
+    exit;
+}
 function protocol()
 {
     return stripos(
@@ -67,5 +99,12 @@ function flash($name, $value = null)
     } else {
         $_SESSION['flash_message'][$name] = $value;
     }
+}
+function dd($var)
+{
+    echo '<pre/>';
+    var_dump($var);
+    exit;
+
 }
 ?>
